77115950c7a8a239adbe8a132853a545cf3b36c8
Arif Shariar Rahman <1705095@ugrad.cse.buet.ac.bd>
2023-02-07 01:08:58 +0600

    Added plots for loss vs accuracy

--- benchmark_codes/plot_benchmark.py
+++ benchmark_codes/plot_benchmark.py
@@ -4,14 +4,25 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def plot_graph(path_to_csv: str = "../benchmark/resnet-101-history.csv"):
+def plot_loss_accuracy(path_to_csv: str = "../benchmark/resnet-101-history.csv"):
     """
     Plot the graph of accuracy and loss
     :param path_to_csv: path to the csv file
     :return: graph
     """
-    print("Plotting the graph... epoch: {}". format(path_to_csv))
+    # read csv file
+    loss = pd.read_csv(path_to_csv, usecols=["loss"])
+    accuracy = pd.read_csv(path_to_csv, usecols=["accuracy"])
+
+    # plot loss
+    plt.plot(loss, label="loss")
+    plt.plot(accuracy, label="accuracy")
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
+    plt.legend(loc='lower right')
+    plt.savefig("../benchmark/resnet-152-epoch-20-non-enhanced-loss-accuracy.png")
+    plt.show()
 
 
 if __name__ == "__main__":
-    plot_graph()
+    plot_loss_accuracy("E:\\Pycharm\\ResNetEvaluations\\benchmark\\resnet-152-epoch-20-history-non-enhanced.csv")
